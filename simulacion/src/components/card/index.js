5d79be1783ad92d15f6018d4d0267a24dcedbd55
sjponce <santiago.j.ponce@gmail.com>
2021-03-28 14:59:53 -0300

    inputs

--- simulacion/src/components/card/index.js
+++ simulacion/src/components/card/index.js
@@ -4,6 +4,9 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 import Chart from 'chart.js';
 import './_shared.css';
 
@@ -11,13 +14,16 @@ const Card = () => {
     
     const [interval, setInterval] = React.useState('');
     const [datos, setDatos] = useState(null);
+    const [x, setX] = useState(null);
+    const [k, setK] = useState(null);
+    const [n, setN] = useState(null);
+    const [g, setG] = useState(null);
+    const [c, setC] = useState(null);
     window.google.charts.load('current', { packages: ['corechart'] });
-    useEffect(()=> {
-    window.google.charts.setOnLoadCallback(drawChart);
-}, []);
 
 
 const [chart, setChart] = useState(null);
+const [method, setMethod] = useState(null);
 function drawChart() {
     var ctx = document.getElementById('myChart');
     chart?.destroy()
@@ -46,56 +52,107 @@ function drawChart() {
         }
     }));
 }
+/* 
+n: 1000, x: 17, k: 2, g:5, intervalos: interval
+n: 1000, x: 6, k: 3, c: 7, g:3, intervalos: interval
+n: 1000, intervalos: interval
+*/
+    useEffect(()=> {
+        drawChart();
+    }, [datos])
+    
+    const handleChangeInterval = (event) => {
+        setInterval(event.target.value);
+      };
 
-useEffect(()=> {
-    ApiRequest.get(`/full-random`, {n: 1000, intervalos: interval}).then(async ({ data }) => {
-        console.log(data);
-        let tableData = data.map(dato => JSON.parse(dato)).map(d => {
-            return [d.frecuencia ? d.frecuencia : 0, d.cota_superior ]
-        });
-        
-        let tableDataList = [...tableData]
-        setDatos(tableDataList);
-    })
-    }, [interval])
+    const handleChangeMethod = (event) => {
+      setMethod(event.target.value);
+    };
 
-useEffect(()=> {
-    drawChart();
-}, [datos])
+    const handleChangeN = (event) => {
+        setN(event.target.value);
+      };
 
-const handleChange = (event) => {
-    setInterval(event.target.value);
-    /* ApiRequest.get(`/congruencial-lineal`, {n: 1000, x: 6, k: 3, c: 7, g:3, intervalos: event.target.value}).then(async ({ data }) => {
-        let tableData = data.map(dato => JSON.parse(dato)).map(d => {
-            return [d.frecuencia ? d.frecuencia : 0, d.cota_superior ]
-        });
-        
-        let tableDataList = [...tableData]
-        setDatos(tableDataList);
-    })  */
-        /* ApiRequest.get(`/congruencial-multiplicativo`, {n: 1000, x: 17, k: 2, g:5, intervalos: event.target.value}).then(async ({ data }) => {
-            let tableData = data.map(dato => JSON.parse(dato)).map(d => {
-                return [d.frecuencia ? d.frecuencia : 0, d.cota_superior ]
-            });
-            
-            let tableDataList = [...tableData]
-            setDatos(tableDataList);
-        }) */
-        
+    const handleChangeX = (event) => {
+        setX(event.target.value);
+      };
+
+      const handleChangeK = (event) => {
+        setK(event.target.value);
+      };
+
+      const handleChangeC = (event) => {
+        setC(event.target.value);
+      };
+
+      const handleChangeG = (event) => {
+        setG(event.target.value);
+      };
+
+      const handleChangeSend = (event) => {
+        switch (method) {
+            case 'full-random':
+                ApiRequest.get('/full-random', {n, intervalos: interval}).then(async ({ data }) => {
+                    let tableData = data.map(dato => JSON.parse(dato)).map(d => {
+                        return [d.frecuencia ? d.frecuencia : 0, d.cota_superior ]
+                    });
+                    
+                    let tableDataList = [...tableData]
+                    setDatos(tableDataList);
+                })
+                break;
+            case 'congruencial-lineal':
+                ApiRequest.get(`/congruencial-lineal`, {n, x, k, c, g, intervalos: interval}).then(async ({ data }) => {
+                    let tableData = data.map(dato => JSON.parse(dato)).map(d => {
+                        return [d.frecuencia ? d.frecuencia : 0, d.cota_superior ]
+                    });
+                    
+                    let tableDataList = [...tableData]
+                    setDatos(tableDataList);
+                })
+                break;
+            case 'congruencial-multiplicativo':
+                ApiRequest.get(`/congruencial-multiplicativo`, {n, x, k, g, intervalos: interval}).then(async ({ data }) => {
+                    let tableData = data.map(dato => JSON.parse(dato)).map(d => {
+                        return [d.frecuencia ? d.frecuencia : 0, d.cota_superior ]
+                    });
+                    
+                    let tableDataList = [...tableData]
+                    setDatos(tableDataList);
+                })    
+                break;
+            default:
+                break;
+        }
       };
 
     return (
-        <div style={{ textAlign: '-webkit-center' }}>
-            <canvas id="myChart" className="chart" width="400" height="400"></canvas>
-            <FormControl>
-            <InputLabel id="demo-simple-select-label">Intervalos</InputLabel>
+        <div class="column">
+            <canvas id="myChart" className="chart"></canvas>
+            <div class="row">
+            <FormControl className="select">
+            <InputLabel id="method-select-label">Método</InputLabel>
+            <Select
+              labelId="method-select-label"
+              id="method-select"
+              value={method}
+              onChange={handleChangeMethod}
+              style={{ width: 200 }}
+              >
+              <MenuItem value={'congruencial-lineal'}>Congruencial lineal</MenuItem>
+              <MenuItem value={'congruencial-multiplicativo'}>Congruencial multiplicativo</MenuItem>
+              <MenuItem value={'full-random'}>Random</MenuItem>
+            </Select>
+            </FormControl>
+            <FormControl className="select">
+            <InputLabel id="intervalos-select-label">Intervalos</InputLabel>
 
             <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
+              labelId="intervalos-select-label"
+              id="intervalos-select"
               value={interval}
-              onChange={handleChange}
-              style={{ width: 100 }}
+              onChange={handleChangeInterval}
+              style={{ width: 200 }}
               >
               <MenuItem value={10}>10</MenuItem>
               <MenuItem value={15}>15</MenuItem>
@@ -103,6 +160,17 @@ const handleChange = (event) => {
             </Select>
             </FormControl>
         </div>
+        <div class="row">
+            <TextField id="outlined-basic" label="Tamaño de muestra (n)" variant="outlined" value={n} onChange={handleChangeN}/>
+            <TextField id="outlined-basic" label="Valor de la semilla (x)" variant="outlined" value={x} onChange={handleChangeX}/>
+            <TextField id="outlined-basic" label="Valor de K" variant="outlined" value={k} onChange={handleChangeK}/>
+            <TextField id="outlined-basic" label="Valor de G" variant="outlined" value={g} onChange={handleChangeG}/>
+            <TextField id="outlined-basic" label="Valor de C" variant="outlined" value={c} onChange={handleChangeC}/>
+        </div>
+                   <Button variant="contained" color="primary" onClick={handleChangeSend}>
+                        Enviar
+                   </Button>
+    </div>
     );
 };
 
