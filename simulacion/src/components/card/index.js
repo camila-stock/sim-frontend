3a4a7af152a0e3dcc1a51cc5395ad93c03ce7521
sjponce <santiago.j.ponce@gmail.com>
2021-03-28 13:56:51 -0300

    flicker fix

--- simulacion/src/components/card/index.js
+++ simulacion/src/components/card/index.js
@@ -8,58 +8,80 @@ import Chart from 'chart.js';
 import './_shared.css';
 
 const Card = () => {
-
+    
+    const [interval, setInterval] = React.useState('');
     const [datos, setDatos] = useState(null);
-useEffect(()=> {
     window.google.charts.load('current', { packages: ['corechart'] });
+    useEffect(()=> {
     window.google.charts.setOnLoadCallback(drawChart);
 }, []);
 
 
 const [chart, setChart] = useState(null);
-    function drawChart() {
-        var ctx = document.getElementById('myChart');
-        if (chart) chart.destroy()
-        setChart(new Chart(ctx, {
-          type: 'bar',
-          data: {
-              labels: datos?.map(d => d[1]),
-              datasets: [{
-                  label: '# of Votes',
-                  data: datos?.map(d => d[0]),
-                  backgroundColor: 
-                  datos?.map(d => 'rgba(54, 162, 235, 0.2)'),
-                  borderColor:
-                    datos?.map(d => 'rgba(54, 162, 235, 1)'),
-                  borderWidth: 1
-              }]
-          },
-          options: {
-              scales: {
-                  yAxes: [{
-                      ticks: {
-                          beginAtZero: true
-                      }
-                  }]
-              }
-          }
-      }));
-    }
+function drawChart() {
+    var ctx = document.getElementById('myChart');
+    chart?.destroy()
+    setChart(new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: datos?.map(d => d[1]),
+            datasets: [{
+                label: 'Frecuencias',
+                data: datos?.map(d => d[0]),
+                backgroundColor: 
+                datos?.map(d => 'rgba(54, 162, 235, 0.2)'),
+                borderColor:
+                datos?.map(d => 'rgba(54, 162, 235, 1)'),
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    }));
+}
 
-    const [interval, setInterval] = React.useState('');
-    
-      const handleChange = (event) => {
-        setInterval(event.target.value);
-        ApiRequest.get(`/congruencial-lineal`, {n: 1000, x: 6, k: 3, c: 7, g:3, intervalos: event.target.value}).then(async ({ data }) => {
+useEffect(()=> {
+    ApiRequest.get(`/full-random`, {n: 1000, intervalos: interval}).then(async ({ data }) => {
+        console.log(data);
+        let tableData = data.map(dato => JSON.parse(dato)).map(d => {
+            return [d.frecuencia ? d.frecuencia : 0, d.cota_superior ]
+        });
+        
+        let tableDataList = [...tableData]
+        setDatos(tableDataList);
+    })
+    }, [interval])
+
+useEffect(()=> {
+    drawChart();
+}, [datos])
+
+const handleChange = (event) => {
+    setInterval(event.target.value);
+    /* ApiRequest.get(`/congruencial-lineal`, {n: 1000, x: 6, k: 3, c: 7, g:3, intervalos: event.target.value}).then(async ({ data }) => {
+        let tableData = data.map(dato => JSON.parse(dato)).map(d => {
+            return [d.frecuencia ? d.frecuencia : 0, d.cota_superior ]
+        });
+        
+        let tableDataList = [...tableData]
+        setDatos(tableDataList);
+    })  */
+        /* ApiRequest.get(`/congruencial-multiplicativo`, {n: 1000, x: 17, k: 2, g:5, intervalos: event.target.value}).then(async ({ data }) => {
             let tableData = data.map(dato => JSON.parse(dato)).map(d => {
                 return [d.frecuencia ? d.frecuencia : 0, d.cota_superior ]
             });
             
             let tableDataList = [...tableData]
-            console.log(tableDataList);
-            setDatos(tableDataList)
-            drawChart();
-        })
+            setDatos(tableDataList);
+        }) */
+        
       };
 
     return (
